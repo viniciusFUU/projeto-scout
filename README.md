78c5bbab1c1eeff016ac7829a6e548eda925b185
Vinícius Monteiro <vinicius3_1998@hotmail.com>
2024-12-05 12:00:48 -0300

    scout

---
+++ README.md
@@ -0,0 +1,8 @@
+projeto de scout futebol amador
+
+entidades:
+estatistica: idEstatistica, estatisticaDescricao;
+campeonato: idCampeonato, nomeCampeonato, qtdTimes, idTime;
+jogador:  idTime, numeroJogador, nomeJogador, idadeJogador, alturaJogador, pesoJogador;
+time: idTime, nomeTime, nomeCampeonato, qtdJogadores;
+dadosJogos: idCampeonato, idTime, idJogador, idEstatistica;
---
+++ ddl.sql
@@ -0,0 +1,57 @@
+create database scout;
+use scout;
+
+create table statistic(
+	statisticId int auto_increment primary key,
+    statisticDescription varchar(25) not null
+);
+
+create table team(
+	teamId int auto_increment primary key,
+    teamName varchar(70) not null,
+    qtdPlayers int not null,
+    firstColor varchar(10),
+    secondColor varchar(10)
+);
+
+create table player(
+	playerId int auto_increment primary key,
+    playerName varchar(55),
+    playerBirthday date,
+    playerWeigth decimal(3,2),
+    playerHeight decimal(3,2),
+    playerNumber int not null
+);
+
+create table team_player(
+	teamPlayerId int auto_increment primary key,
+    teamId int,
+    playerId int,
+    foreign key (teamId) references team(teamId),
+    foreign key (playerId) references player(playerId)
+);
+
+create table championship(
+	championshipId int auto_increment primary key,
+    championshipName varchar(50) not null,
+    qtdTeams int not null
+);
+
+create table team_championship(
+	teamChampionshipId int auto_increment primary key,
+	teamId int,
+    championshipId int,
+    qtdInserido int,
+    foreign key (teamId) references team(teamId),
+    foreign key (championshipId) references championship(championshipId)
+);
+
+create table match_stats(
+	matchStatusId int auto_increment primary key,
+    teamPlayerId int,
+	teamChampionshipId int,
+    statisticId int,
+    foreign key (teamPlayerId) references team_player(teamPlayerId),
+    foreign key (teamChampionshipId) references team_championship(teamChampionshipId),
+    foreign key (statisticId) references statistic(statisticId)
+);
