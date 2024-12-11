c9487f5e5d79a1e09514f8bcfc113288e9bc400b
Vinícius Monteiro <vinicius3_1998@hotmail.com>
2024-12-11 09:09:36 -0300

    Criação entidade match

---
+++ scoutProject/backend/src/main/java/project/scout/model/Match.java
@@ -0,0 +1,39 @@
+package project.scout.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "match")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Match {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "matchId")
+    private int matchId;
+
+    @ManyToOne
+    @JoinColumn(name = "championshipId")
+    private Championship championshipId;
+
+    @ManyToOne
+    @JoinColumn(name = "teamHome")
+    private Team teamHome;
+
+    @ManyToOne
+    @JoinColumn(name = "teamVisit")
+    private Team teamVisit;    
+}
